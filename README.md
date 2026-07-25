0bb4cd2a649be69ab4ac6c1eee561a2a7d3ba443
mariomadproductions <9919020+mariomadproductions@users.noreply.github.com>
2026-07-25 21:02:17 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# py-util
+
+Contains utilities I use in my Python projects.
+
+## Functions
+### open
+Wrapper for the builtin `open`, that has special behaviour if the file path is "-", for stdin and stdout handling. Based on https://stackoverflow.com/questions/1744989/read-from-file-or-stdin/29824059#29824059
+
---
+++ src/util/__init__.py
@@ -0,0 +1,18 @@
+import sys
+import contextlib
+import builtins
+
+@contextlib.contextmanager
+def open(file, mode, *args, **kwargs):
+    if file == '-':
+        if mode is None or mode == '' or 'r' in mode:
+            fh = sys.stdin
+        else:
+            fh = sys.stdout
+    else:
+        fh = builtins.open(file, mode)
+    try:
+        yield fh
+    finally:
+        if file != '-':
+            fh.close()
